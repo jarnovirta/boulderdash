d7bc35f6b670b14c599234f5ce2b393fab6c5b35
Jarno Virta <jarnovirta@users.noreply.github.com>
2019-05-07 22:42:30 +0300

    Cleanup

--- src/boulderdash/service/ImageService.java
+++ src/boulderdash/service/ImageService.java
@@ -12,15 +12,15 @@ import javafx.scene.image.Image;
  * @author Jarno
  */
 public class ImageService {
-    public static Image BRICK_WALL_HORIZONTAL = new Image("file:brown_brick_horizontal.jpg");
-    public static Image BRICK_WALL_VERTICAL = new Image("file:brown_brick_vertical.jpg");    
-    public static Image HERO_LEFT = new Image("file:rockford_left.jpg");
-    public static Image HERO_RIGHT = new Image("file:rockford_right.jpg");
-    public static Image HERO_FORWARD = new Image("file:rockford_forward.jpg");
-    public static Image HERO_BLINK = new Image("file:rockford_blink.jpg");
-    public static Image DIAMOND = new Image("file:diamond.jpg");
-    public static Image ROCK = new Image("file:rock.jpg");
-    public static Image DIRT = new Image("file:dirt.jpg");
-    public static Image TUNNEL = new Image("file:black_square.jpg");
+    public static final Image BRICK_WALL_HORIZONTAL = new Image("file:brown_brick_horizontal.jpg");
+    public static final Image BRICK_WALL_VERTICAL = new Image("file:brown_brick_vertical.jpg");    
+    public static final Image HERO_LEFT = new Image("file:rockford_left.jpg");
+    public static final Image HERO_RIGHT = new Image("file:rockford_right.jpg");
+    public static final Image HERO_FORWARD = new Image("file:rockford_forward.jpg");
+    public static final Image HERO_BLINK = new Image("file:rockford_blink.jpg");
+    public static final Image DIAMOND = new Image("file:diamond.jpg");
+    public static final Image ROCK = new Image("file:rock.jpg");
+    public static final Image DIRT = new Image("file:dirt.jpg");
+    public static final Image TUNNEL = new Image("file:black_square.jpg");
     
 }
